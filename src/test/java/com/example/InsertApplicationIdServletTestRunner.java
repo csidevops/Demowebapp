8f10945a8009650c3883203a55009c4f886760dd
csidevops <56381436+csidevops@users.noreply.github.com>
2023-08-07 06:57:21 +0000

    Update InsertApplicationIdServletTestRunner.java

--- src/test/java/com/example/InsertApplicationIdServletTestRunner.java
+++ src/test/java/com/example/InsertApplicationIdServletTestRunner.java
@@ -1,16 +1,15 @@
-import org.junit.Test;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
 public class InsertApplicationIdServletTestRunner {
-   public static void main(String[] args) {
-      Result result = JUnitCore.runClasses(ManageBeanTest.class);
-		
-      for (Failure failure : result.getFailures()) {
-         System.out.println(failure.toString());
-      }
-		
-      System.out.println(result.wasSuccessful());
-   }
-}  
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(InsertApplicationIdServletTest.class);
+
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+
+        System.out.println(result.wasSuccessful());
+    }
+}
