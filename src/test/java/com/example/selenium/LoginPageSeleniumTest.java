2f00c254e94ab2caa7a30db5621428283c7ad75d
csidevops <56381436+csidevops@users.noreply.github.com>
2023-08-07 11:08:44 +0000

    Update and rename LoginPageTest.java to LoginPageSeleniumTest.java

--- src/test/java/com/example/selenium/LoginPageSeleniumTest.java
+++ src/test/java/com/example/selenium/LoginPageSeleniumTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.junit.Test; // Import the JUnit Test annotation
 
-public class LoginPageTest {
+public class LoginPageSeleniumTest {
     @Test
     public void testLoginPage() {
         // Set the path to the ChromeDriver executable
