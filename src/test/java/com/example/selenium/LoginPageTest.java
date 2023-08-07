9a6b1a2caaa859b084e08e56ee77eb4949f9ae5c
csidevops <56381436+csidevops@users.noreply.github.com>
2023-08-07 08:53:00 +0000

    Create LoginPageTest.java

---
+++ src/test/java/com/example/selenium/LoginPageTest.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class HeadlessSeleniumTest {
+    public static void main(String[] args) {
+        // Set the path to the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "C:\Users\csidevops\ChromeDriver\chromedriver-win64");
+
+        // Configure ChromeOptions for headless mode
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+
+        // Initialize WebDriver
+        WebDriver driver = new ChromeDriver(options);
+
+        // Open the URL
+        driver.get("http://localhost:8080/Demowebapp"); // Replace with your web portal URL
+
+        // Check if the page title contains a keyword
+        String pageTitle = driver.getTitle();
+        if (pageTitle.contains("Demo Web App")) { // Replace "Example" with your keyword
+            System.out.println("Test passed: Web portal appeared successfully.");
+        } else {
+            System.out.println("Test failed: Web portal did not appear as expected.");
+        }
+
+        // Close the browser
+        driver.quit();
+    }
+}
