a6cd84e3b45b1bb578a162c746310f711b29cb6e
csidevops <56381436+csidevops@users.noreply.github.com>
2023-08-07 06:40:32 +0000

    Update InsertApplicationIdServletTestRunner.java

--- src/test/java/com/example/InsertApplicationIdServletTestRunner.java
+++ src/test/java/com/example/InsertApplicationIdServletTestRunner.java
@@ -2,7 +2,7 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
-public class Runner {
+public class InsertApplicationIdServletTestRunner {
    public static void main(String[] args) {
       Result result = JUnitCore.runClasses(ManageBeanTest.class);
 		
