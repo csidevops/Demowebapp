6433cb1ed9e794e984db7f95888d7aad07230d3b
csidevops <56381436+csidevops@users.noreply.github.com>
2023-08-07 06:55:04 +0000

    Update InsertApplicationIdServletTest.java

--- src/test/java/com/example/InsertApplicationIdServletTest.java
+++ src/test/java/com/example/InsertApplicationIdServletTest.java
@@ -1,6 +1,5 @@
 package com.example;
-//package test.easycodeforall.changeit; 
-//import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.apache.log4j.Logger;
 import javax.servlet.ServletException;
@@ -9,25 +8,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
 public class InsertApplicationIdServletTest {
   
-	private Logger log = Logger.getLogger(this.getClass());
- 
-  @Test  
-    public void doPost(){  
-		try {
-			log.info("Starting execution of doPost");
- HttpServletRequest request = null; 
-HttpServletResponse response = null; 
- 
-  
- InsertApplicationIdServlet insertapplicationidservlet  =new InsertApplicationIdServlet(); 
-insertapplicationidservlet.doPost( request ,response);
-assertTrue(true);
-		} catch (Exception exception) {
-			log.error("Exception in execution ofdoPost-"+exception,exception);
-			exception.printStackTrace();
-			assertFalse(false);
-		}
-    }  
+    private Logger log = Logger.getLogger(this.getClass());
+
+    @Test
+    public void doPost() {
+        try {
+            log.info("Starting execution of doPost");
+            HttpServletRequest request = null;
+            HttpServletResponse response = null;
+
+            InsertApplicationIdServlet insertapplicationidservlet = new InsertApplicationIdServlet();
+            insertapplicationidservlet.doPost(request, response);
+            assertTrue(true);
+        } catch (Exception exception) {
+            log.error("Exception in execution of doPost-" + exception, exception);
+            exception.printStackTrace();
+            assertFalse(false);
+        }
+    }
 }
